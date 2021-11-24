4365e01f288dc72d005247d3a7f74975b780b0aa
bautiabella <abautista@udesa.edu.ar>
2021-11-24 12:12:08 -0300

    Like con error

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,90 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity, Modal } from 'react-native'
+import { auth, db } from '../firebase/config';
+import firebase from 'firebase';
 
-export default function Post ({item}){
+export default class Post extends Component{
 
-    console.log(item);
+    constructor(props){
+        super(props);
+        this.state = {
+            liked: false,
+            likes: 0,
+            showModal: false,
+        }
+    }
 
-    return(
-        <View stlye={styles.container}>
-            <Text>{item.data.description}</Text>
-            <Text>{item.data.createdAt}</Text>
-            <Text>{item.data.owner}</Text>
-        </View>
-    )
+    componentDidMount(){
+        if (this.props.item){
+            if (this.props.item.data.likes.length !== 0){
+                this.setState({
+                    likes: this.props.item.data.likes.length
+                })
+                if (this.props.item.data.likes.includes(auth.currentUser.email)){
+                    this.setState({
+                        liked: true
+                    })
+                }
+            }
+        }
+    }
+
+    onLike(){
+        const posteoActualizar = db.collection('posts').doc(this.props.item.id)
+        
+        posteoActualizar.update({
+            likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+        })
+        .then(()=> {
+            this.setState({
+                liked: true,
+                likes: this.state.likes + 1
+            })
+        })
+    }
+
+    onDislike(){
+        const posteoActualizar = db.collection('posts').doc(this.props.item.id)
+        
+        posteoActualizar.update({
+            likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+        })
+        .then(()=> {
+            this.setState({
+                liked: false,
+                likes: this.state.likes - 1
+            })
+        })
+    }
+    
+    render(){
+
+        console.log(this.props.item);
+        return(
+            <View stlye={styles.container}>
+                <Text>{this.props.item.data.description}</Text>
+                <Text>{this.props.item.data.createdAt}</Text>
+                <Text>{this.props.item.data.owner}</Text>
+                <Text>Likes: {this.state.likes}</Text>
+                {
+                    !this.state.liked ?
+                    <TouchableOpacity onPress = {()=> this.onLike()}>
+                        <Text>
+                            Like
+                        </Text>
+                    </TouchableOpacity>
+                    :
+                    <TouchableOpacity onPress = {()=> this.onDislike()}>
+                        <Text>
+                            Unlike
+                        </Text>
+                    </TouchableOpacity>
+                }
+            </View>
+        )
+    }
 }
+
 
 const styles = StyleSheet.create({
     image: {
